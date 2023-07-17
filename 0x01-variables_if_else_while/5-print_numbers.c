597e6131b4c5b9d546b8ece2319e236ddf4ef7f5
ZevileKhumalo <zevilekhumalo3@gmail.com>
2023-07-17 04:06:46 -0700

    .

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - prints all single digit number of the base of 10
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
